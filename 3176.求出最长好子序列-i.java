77537e696140c290d7f425f2c65dcdc04df2f507
SkyDependence <nanyangzesi@gmail.com>
2024-09-06 20:11:46 +0800

    can't finish, i dont know how to use DP

--- 3176.求出最长好子序列-i.java
+++ 3176.求出最长好子序列-i.java
@@ -3,30 +3,40 @@
  *
  * [3176] 求出最长好子序列 I
  */
-
+import java.util.*;;
 // @lc code=start
 class Solution {
     public int maximumLength(int[] nums, int k) {
         int count = 1;
-        int pair = 0;
-        int pre = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (pair == k) {
-                for (; i < nums.length; i++) {
-                    if (nums[i] == pre) {
-                        count++;
-                    }
+        int changes = 0;
+        int left = 0;
+        if (k == 0) {
+            HashMap<Integer, Integer> countMap = new HashMap<>();
+            for (int num : nums) {
+                countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+                if (entry.getValue() > count) {
+                    count = entry.getValue();
                 }
-                break;
             }
-            if (pre == nums[i]) {
-                count++;
+
+            return count;
+        }
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[right] != nums[right - 1]) {
+                changes++;
             }
-            if (pre != nums[i] && pair < k) {
-                count++;
-                pair++;
-                pre = nums[i];
+
+            while (changes > k) {
+                if (nums[left] != nums[left + 1]) {
+                    changes--;
+                }
+                left++;
             }
+
+            count = Math.max(count, right - left + 1);
         }
         return count;
     }
