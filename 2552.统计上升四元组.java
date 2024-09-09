771be9a82df6a64e2679e067e5cb979a0b7e7e08
SkyDependence <nanyangzesi@gmail.com>
2024-09-10 07:30:56 +0800

    can't finish

---
+++ 2552.统计上升四元组.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2552 lang=java
+ *
+ * [2552] 统计上升四元组
+ */
+
+// @lc code=start
+class Solution {
+    public long countQuadruplets(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
