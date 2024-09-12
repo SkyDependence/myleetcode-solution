28a8d4ddf23984da3cd20a975bbe05c19cea6643
SkyDependence <nanyangzesi@gmail.com>
2024-09-13 07:37:05 +0800

    create

---
+++ 2398.预算内的最多机器人数目.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2398 lang=java
+ *
+ * [2398] 预算内的最多机器人数目
+ */
+
+// @lc code=start
+class Solution {
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+
+    }
+}
+// @lc code=end
+
