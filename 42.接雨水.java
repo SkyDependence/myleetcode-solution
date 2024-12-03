363a78561bf73947c0da848df49d28eaecdbfe2c
SkyDependence <nanyangzesi@gmail.com>
2024-12-03 16:19:04 +0800

    ✨ feat:

--- 42.接雨水.java
+++ 42.接雨水.java
@@ -7,7 +7,27 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
-        
+        int left = 0;
+        int right = height.length - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int water = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] > maxLeft) {
+                    maxLeft = height[left];
+                }
+                water += maxLeft - height[left];
+                left++;
+            } else {
+                if (height[right] > maxRight) {
+                    maxRight = height[right];
+                }
+                water += maxRight - height[right];
+                right--;
+            }
+        }
+        return water;
     }
 }
 // @lc code=end
