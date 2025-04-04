2b2a43b5b6cdd155648297edc8ef0655a69fbdaa
SkyDependence <nanyangzesi@gmail.com>
2025-04-04 19:30:29 +0800

    ✨ feat: 优化移除元素算法，改进元素删除逻辑

--- 27.移除元素.java
+++ 27.移除元素.java
@@ -7,7 +7,21 @@
 // @lc code=start
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
+        int count = 0;
+        int l = nums.length;
+        int index = 0;
+        for (int i = 0; i < l - index; i++) {
+            if (nums[i] == val) {
+                for (int j = i; j < l - 1; j++) {
+                    nums[j] = nums[j + 1];
+                }
+                index++;
+                count++;
+                if (nums[i] == val)
+                    i--;
+            }
+        }
+        return l - count;
     }
 }
 // @lc code=end
