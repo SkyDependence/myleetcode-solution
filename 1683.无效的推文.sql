696e06787484319d6e8f182997c9594b89bb0aad
SkyDependence <nanyangzesi@gmail.com>
2025-04-07 15:02:25 +0800

    ✨ feat: 添加无效的推文 MySQL 查询解决方案

---
+++ 1683.无效的推文.sql
@@ -0,0 +1,11 @@
+--
+-- @lc app=leetcode.cn id=1683 lang=mysql
+--
+-- [1683] 无效的推文
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT tweet_id FROM Tweets WHERE CHAR_LENGTH(content) > 15;
+-- @lc code=end
+
